a0675ec190992a8c2e0fbebab9d645e0a0d456ef
vsrzone <vikumsri@gmail.com>
2015-03-20 04:53:38 +0530

    late night commit, cant remember shitzzzzzzzzzz

---
+++ aviva.php
@@ -0,0 +1,178 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NIMO & co</title>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
+</head>
+<body>
+
+<div id="page-wrapper">
+	<div class="container-fluid top-navigation">
+	 	<div class="row centeral">
+	 		<ul class="main-menu">
+		 		<li class="abc nopadding-l"><a href="index.php">HOME</a></li>
+		 		<li class="abc"><a href="about.php">ABOUT</a></li>
+		 		<li class="abc"><a href="products.php">PRODUCTS</a></li>
+		 	</ul>
+		 	<ul class="main-menu">
+		 		<li class="abc"><a href="/"><img id="logo" src="images/logo.jpg" height="25px" width="auto" alt="Nimo & co"></a>
+		 	</ul>
+		 	<ul class="main-menu">
+		 		<li class="abc"><a href="clients.php">CLIENTS</a></li>
+		 		<li class="abc"><a href="news.php">NEWS</a></li>
+		 		<li class="abc"><a href="contact.php">CONTACT</a></li>
+	 		</ul>
+	 	</div>
+	</div>
+
+	<div class="container-fluid padding-t-30 slideshow">
+		<div class="row">
+			<div id="top-banner">
+				<img class="page-banner"src="images/banners/about.png" height="250px" width="auto" alt="" title="banner01">
+				<div id="page-title-wrapper">
+					<p id="page-title">Aviva</p>
+					<p id="breadcrums"><a href="clients.php" target="_self">Clients</a> &gt; <a href="aviva.php" target="_self">Aviva</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid about-content">
+		<div class="row">
+			<div class="col-xs-3">
+				<ul>
+					<a href="clients.php"><li class="side-menu" id="side-heading">Clients</li></a>
+					<a href="aviva.php"><li class="side-menu side-menu-selected">Aviva</li></a>
+					<a href="aviva.php"><li class="side-menu">BOC</li></a>
+					<a href="aviva.php"><li class="side-menu">HSBC</li></a>
+					<a href="aviva.php"><li class="side-menu">Colombo Jwellery Store</li></a>
+				</ul>
+			</div>
+			<div class="col-xs-9">
+				<div class="about-info">
+					<p class="about-para"><img src="images/vision.png" width="150px" alt="vision" title="vision">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare magna quis velit ullamcorper faucibus et vitae ligula. Curabitur dolor felis, tristique eu malesuada sollicitudin, cursus eu risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+					<h5>SOME TITLE</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare magna quis velit ullamcorper faucibus et vitae ligula. Curabitur dolor felis, tristique eu malesuada sollicitudin, cursus eu risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur imperdiet pulvinar erat, et suscipit ipsum tincidunt eget.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare magna quis velit ullamcorper faucibus et vitae ligula.</p>
+					<div class="flexslider">
+						<ul class="slides">
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe1.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe2.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe1.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe2.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe1.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+							<li class="award">
+								<div class="award-img-wrapper">
+									<img src="images/chubbsafes/chubbsafe.png" alt="brand" title="chubbsafes" width="100%" height="auto">
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container-fluid footer">
+			<div class="row">
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">BRANDS</a></li>
+					<li class="footer-desc"><a href="#">Barcelona</a></li>
+					<li class="footer-desc"><a href="#">Velo</a></li>
+					<li class="footer-desc"><a href="#">Delphi</a></li>
+					<li class="footer-desc"><a href="#">Fulkrum</a></li>
+					<li class="footer-desc"><a href="#">Wau</a></li>
+				</ul>
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">PRODUCTS</a></li>
+					<li class="footer-desc"><a href="#">Chairs</a></li>
+					<li class="footer-desc"><a href="#">Safes</a></li>
+					<li class="footer-desc"><a href="#">Furnitures</a></li>
+					<li class="footer-desc"><a href="#">Locks</a></li>
+					<li class="footer-desc"><a href="#">Tables</a></li>
+				</ul>
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">LEGAL</a></li>
+					<li class="footer-desc"><a href="#">Privacy Policy</a></li>
+					<li class="footer-desc"><a href="#">Terms and Conditions</a></li>
+					<li class="footer-desc"><a href="#">Cancellations</a></li>
+					<li class="footer-desc"><a href="#">Order Process</a></li>
+				</ul>
+				<div class="col-xs-6" id="copyrights">
+					<div class="footer-title">&nbsp;</div>
+					<p class="footer-desc">Copyright &copy; NIMO & CO 2015</p>
+					<p class="footer-desc">Build By : <a href="#" target="_blank">Pyxle</a></p>
+					<div class="footer-desc footer-icons">
+						<a href="#" target="_blank"><img class="footer-icon" src="images/fb.png" alt="facebook" title="facebook" id="fb-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/twitter.png" alt="twitter" title="twitter" id="twitter-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/instagram.png" alt="instagram" title="instagram" id="instagram-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/gplus.png" alt="google plus" title="google plus" id="gplus-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/pinterest.png" alt="pinterest" title="pinterest" id="pinterest-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/linkedin.png" alt="linkedin" title="linkedin" id="linkedin-icon"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script defer src="js/jquery.flexslider-min.js"></script>
+
+  <script type="text/javascript">
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 2,
+        pausePlay: false,
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+  </script>
+
+</html>
---
+++ chubbsafes.php
@@ -0,0 +1,121 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NIMO & co</title>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+</head>
+<body>
+
+<div id="page-wrapper">
+	<div class="container-fluid top-navigation">
+	 	<div class="row centeral">
+	 		<ul class="main-menu">
+		 		<li class="abc nopadding-l"><a href="index.php">HOME</a></li>
+		 		<li class="abc"><a href="about.php">ABOUT</a></li>
+		 		<li class="abc"><a href="products.php">PRODUCTS</a></li>
+		 	</ul>
+		 	<ul class="main-menu">
+		 		<li class="abc"><a href="/"><img id="logo" src="images/logo.jpg" height="25px" width="auto" alt="Nimo & co"></a>
+		 	</ul>
+		 	<ul class="main-menu">
+		 		<li class="abc"><a href="clients.php">CLIENTS</a></li>
+		 		<li class="abc"><a href="news.php">NEWS</a></li>
+		 		<li class="abc"><a href="contact.php">CONTACT</a></li>
+	 		</ul>
+	 	</div>
+	</div>
+
+	<div class="container-fluid padding-t-30 slideshow">
+		<div class="row">
+			<div id="top-banner">
+				<img src="images/banners/awards.jpg" height="300px" width="804px" alt="" title="awards">
+				<div id="page-title-wrapper">
+					<p id="page-title">Chubb Safes</p>
+					<p id="breadcrums"><a href="products.php" target="_self">Products</a> &gt; <a href="chubbsafes.php"  target="_self">Chubb Safes</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid about-content">
+		<div class="row">
+			<div class="col-xs-3">
+				<ul>
+					<a href="about.php"><li class="side-menu" id="side-heading">Products</li></a>
+					<a href="chubsafes.php"><li class="side-menu side-menu-selected">Chub Safes</li></a>
+					<a href="chubsafes.php"><li class="side-menu">Fulcrum</li></a>
+					<a href="chubsafes.php"><li class="side-menu">Vitra</li></a>
+					<a href="chubsafes.php"><li class="side-menu">Herman Miller</li></a>
+				</ul>
+			</div>
+			<div class="col-xs-9">
+				<div class="product-info">
+					<ul>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe.png" alt="brand" title="chubbsafes" width="100%" height="auto"></div></a></li>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe1.png" alt="brand" title="fulcrum" width="100%" height="auto"></div></a></li>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe2.png" alt="brand" title="vitra" width="100%" height="auto"></div></a></li>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe1.png" alt="brand" title="chubbsafes" width="100%" height="auto"></div></a></li>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe2.png" alt="brand" title="fulcrum" width="100%" height="auto"></div></a></li>
+						<li class="award"><a href="chubbsafes.php" target="_self"><div class="award-img-wrapper"><img src="images/chubbsafes/chubbsafe.png" alt="brand" title="vitra" width="100%" height="auto"></div></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container-fluid footer">
+			<div class="row">
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">BRANDS</a></li>
+					<li class="footer-desc"><a href="#">Barcelona</a></li>
+					<li class="footer-desc"><a href="#">Velo</a></li>
+					<li class="footer-desc"><a href="#">Delphi</a></li>
+					<li class="footer-desc"><a href="#">Fulkrum</a></li>
+					<li class="footer-desc"><a href="#">Wau</a></li>
+				</ul>
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">PRODUCTS</a></li>
+					<li class="footer-desc"><a href="#">Chairs</a></li>
+					<li class="footer-desc"><a href="#">Safes</a></li>
+					<li class="footer-desc"><a href="#">Furnitures</a></li>
+					<li class="footer-desc"><a href="#">Locks</a></li>
+					<li class="footer-desc"><a href="#">Tables</a></li>
+				</ul>
+				<ul class="col-xs-2">
+					<li class="footer-title"><a href="#">LEGAL</a></li>
+					<li class="footer-desc"><a href="#">Privacy Policy</a></li>
+					<li class="footer-desc"><a href="#">Terms and Conditions</a></li>
+					<li class="footer-desc"><a href="#">Cancellations</a></li>
+					<li class="footer-desc"><a href="#">Order Process</a></li>
+				</ul>
+				<div class="col-xs-6" id="copyrights">
+					<div class="footer-title">&nbsp;</div>
+					<p class="footer-desc">Copyright &copy; NIMO & CO 2015</p>
+					<p class="footer-desc">Build By : <a href="#" target="_blank">Pyxle</a></p>
+					<div class="footer-desc footer-icons">
+						<a href="#" target="_blank"><img class="footer-icon" src="images/fb.png" alt="facebook" title="facebook" id="fb-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/twitter.png" alt="twitter" title="twitter" id="twitter-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/instagram.png" alt="instagram" title="instagram" id="instagram-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/gplus.png" alt="google plus" title="google plus" id="gplus-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/pinterest.png" alt="pinterest" title="pinterest" id="pinterest-icon"></a>
+						<a href="#" target="_blank"><img class="footer-icon" src="images/linkedin.png" alt="linkedin" title="linkedin" id="linkedin-icon"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/responsiveslides.min.js"></script>
+<script>
+
+</script>
+</html>
